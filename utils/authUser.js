5c26dae19b25cc92ca739a32fee69888ad6b3bf8
malmossa <malmossa@gmail.com>
2021-09-08 19:30:40 -0700

    Still on it

--- utils/authUser.js
+++ utils/authUser.js
@@ -1,1 +1,18 @@
-export const regexUserName = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/;
+import axios from "axios";
+import baseUrl from "./baseUrl";
+import catchErrors form "./catchErrors";
+import Router from "next/router";
+import cookie from "js-cookie";
+
+
+export const registerUser = async (user, ProfilePicUrl, setError, setLoading) => {
+  setLoading(true)
+
+  try {
+    const res = await axios.post(`${baseUrl}/api/signup`, {user, ProfilePicUrl})
+    setToken (res.data)
+  } catch (error) {
+    const errorMsg = catchErrors(error)
+    setError(errorMsg)
+  }
+}
